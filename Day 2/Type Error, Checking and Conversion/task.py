017f1cd7688ad0f8b1ff5e0e0485a8aff13cd269
Robert Pliszka <robplisz@gmail.com>
2026-08-10 20:25:58 -0400

    Type Erro, Type checking and conversion

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,18 @@
-len(12345)
+len("12345")
+
+# String
+print(type("Hello World"))
+# Integer
+print(type(1234))
+# Float
+print(type(123.45))
+# Boolean
+print(type(True))
+
+# String concatination
+print("123" + "456")
+
+# Type conversion and addition
+print(int("123") + int("456"))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
