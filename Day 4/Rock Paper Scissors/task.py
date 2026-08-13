8d0ffcba396056183c84504d32def10a605b5399
Robert Pliszka <robplisz@gmail.com>
2026-08-12 22:53:29 -0400

    Completed Rock Paper Scissors game

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,6 @@
+# Rock Paper Scissors Game
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +27,43 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+# Combining choices into a list.
+choices = [rock, paper, scissors]
+computer_number = random.randint(0, len(choices))
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+
+# prevent user from choosing number out of range.
+if user_choice > 2 or user_choice < 0:
+    print("Please choose a valid number")
+
+if user_choice == computer_number:
+    print("It's a draw")
+    print(choices[computer_number])
+    print(choices[user_choice])
+elif user_choice == 0 and computer_number == 2:
+    print("You win")
+    print(choices[computer_number])
+    print(choices[user_choice])
+elif user_choice == 2 and computer_number == 0:
+    print("You lose")
+    print(choices[computer_number])
+    print(choices[user_choice])
+elif user_choice == 1 and computer_number == 2:
+    print("You loose")
+    print(choices[computer_number])
+    print(choices[user_choice])
+elif user_choice == 2 and computer_number == 1:
+    print("You win")
+    print(choices[computer_number])
+    print(choices[user_choice])
+elif user_choice == 0 and computer_number == 1:
+    print("You loose")
+    print(choices[computer_number])
+    print(choices[user_choice])
+else:
+    print("You win")
+    print(choices[computer_number])
+    print(choices[user_choice])
+
+print(f"Comp: {computer_number}, user: {user_choice}")
