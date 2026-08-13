a6ab4b5dc5083b6b36eed3b1415f472c00e73f0e
Robert Pliszka <robplisz@gmail.com>
2026-08-13 19:41:47 -0400

    Using for loops

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,7 @@
 fruits = ["Apple", "Peach", "Pear"]
+
+# for loop
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,23 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+# sums up integers from list together
+total_exam_score = sum(student_scores)
+
+print(total_exam_score)
+
+# same results using For loop
+sum = 0
+for score in student_scores:
+    sum += score
+
+print(sum)
+
+# prints highest value in list
+print(max(student_scores))
+
+# same result using For loop
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(highest_score)
