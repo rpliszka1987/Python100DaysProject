3e9ce7bc17e29c04f5c4ece2651e0171eb5e9035
Robert Pliszka <robplisz@gmail.com>
2026-08-13 20:14:33 -0400

    Completed password generator

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,6 @@
+# Password generator
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +10,19 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#emptty password
+user_password = []
 
+# Generating random numbers, letters and symbols. Add to user_password list
+for letter in range(1, nr_letters + 1):
+    user_password.append(letters[random.randint(0, len(letters) - 1)])
+
+for symbol in range(1, nr_symbols + 1):
+    user_password.append(symbols[random.randint(0, len(symbols) -1 )])
+
+for number in range(1, nr_numbers + 1):
+    user_password.append(numbers[random.randint(0, len(numbers) - 1)])
+
+# randomise list items for secure password
+random.shuffle(user_password)
+print(str.join("", user_password))
