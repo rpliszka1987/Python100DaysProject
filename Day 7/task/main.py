e7acdb654d26aa61cb6c6092f2641fab607f364e
Robert Pliszka <robplisz@gmail.com>
2026-08-17 16:38:49 -0400

    Hangman created blank spaces for word. Checks and displays letter at the correct position

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,18 +1,27 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Enter a letter : ").lower()
-print(guess)
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+placeholder = ""
+word_length = len(chosen_word)
+for letter in range(word_length):
+    placeholder += "_"
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+print(placeholder)
+
+guess = input("Guess a letter: ").lower()
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+display = ""
+
 for letter in chosen_word:
     if letter == guess:
-        print("Correct")
+        display += letter
     else:
-        print("Wrong")
+        display += "_"
+
+print(display)
+
