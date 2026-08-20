cf3b9f4ae165c9977e4eb3de9e049ca533310460
Robert Pliszka <robplisz@gmail.com>
2026-08-19 22:22:06 -0400

    Created a while loop to keep asking user to guess letters in hangman game

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -4,24 +4,35 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 placeholder = ""
 word_length = len(chosen_word)
-for letter in range(word_length):
+for position in range(word_length):
     placeholder += "_"
-
 print(placeholder)
 
-guess = input("Guess a letter: ").lower()
+# TODO-1: - Use a while loop to let the user guess again.
+#Variable to keep user playing the game
+win = False
+correct_letters = []
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-display = ""
+while not win:
+    guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+    display = ""
 
-print(display)
+    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
 
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        win = True
+        print("You win!")
