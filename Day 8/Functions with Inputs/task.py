b386e0ec646a04b8f44f3c1990183da4520d5e57
Robert Pliszka <robplisz@gmail.com>
2026-08-21 16:27:27 -0400

    Created functions with parameters

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello")
+    print("How are you?")
+    print("Have a good day!")
+
+greet()
+
+# Function with input
+
+def greet_with_name(name):
+    print(f"Hello, {name}!")
+    print(f"How are you today {name}?")
+    print(f"Have a good day {name}!")
+
+greet_with_name("Robert")
