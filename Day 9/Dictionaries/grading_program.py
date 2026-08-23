d00a9906d8c236b1c2815bfd19517ca71175e7b6
Robert Pliszka <robplisz@gmail.com>
2026-08-23 10:44:30 -0400

    Created a grading program using dictionaries

---
+++ Day 9/Dictionaries/grading_program.py
@@ -0,0 +1,22 @@
+# Grading Program
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for key in student_scores:
+    if student_scores[key] > 90 and student_scores[key] <= 100:
+        student_grades[key] = "Outstanding"
+    elif student_scores[key] > 80 and student_scores[key] <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif student_scores[key] > 70 and student_scores[key] <= 80:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+print(student_grades)
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,27 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+# Dictionaries
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+print(programming_dictionary["Bug"])
+
+# Adding to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
+
+# Empty Dictionary
+empty_dictionary = {}
+
+# Wipe existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+# Edit item in dictionary
+programming_dictionary["Loop"] = "This is a new value"
+print(programming_dictionary)
+
+# Loop through dictionary
+for key in programming_dictionary:
+    print(programming_dictionary[key])
+
