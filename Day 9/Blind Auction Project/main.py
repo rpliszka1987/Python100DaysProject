58e27d2fa9b66da409990f2d77e01d7c715e9994
Robert Pliszka <robplisz@gmail.com>
2026-08-23 11:28:06 -0400

    Completed Big Auction Program

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,26 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+# Silent Auction Project
+import art
+
+print(art.logo)
+
+auction_bids = {}
+additional_bids = True
+
+while additional_bids:
+
+    bidder_name = input("What is your name? ")
+    bet_amount = int(input("How much do you bet? "))
 
 
+    auction_bids[bidder_name] = bet_amount
+    additional_bets = input("Are there additional bids? y for yes and n for no: ")
+    if additional_bets == "n":
+        additional_bids = False
+        highest_bid = 0
+        for bid in auction_bids:
+            if auction_bids[bid] > highest_bid:
+                print(f"{bid} won the bid with ${auction_bids[bid]}")
+
+
+    elif additional_bets == "y":
+        print(100 * "\n")
