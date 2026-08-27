7bf0e08b0cabbfdc1e550b7de0c9d2ce448700fe
Robert Pliszka <robplisz@gmail.com>
2026-08-27 18:15:26 -0400

    Completed the Calculator Project

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,40 @@
+# Calculator Project
+import art
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def calculate(num_1,operation, num_2):
+    if mathematical_operation == "+":
+        return calculator_operations["+"](num_1, num_2)
+    elif mathematical_operation == "-":
+        return calculator_operations["-"](num_1, num_2)
+    elif mathematical_operation == "*":
+        return calculator_operations["*"](num_1, num_2)
+    elif mathematical_operation == "/":
+        return calculator_operations["/"](num_1, num_2)
+    else:
+        return "Invalid operation"
+
+calculator_operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+print(art.logo)
+
+first_number = int(input("Enter first number: "))
+mathematical_operation = input("Enter mathematical operation \n + \n - \n * \n / \n : ")
+second_number = int(input("Enter second number: "))
+
+print(calculate(first_number, mathematical_operation, second_number))
--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,7 @@
+# Docstring
 def format_name(f_name, l_name):
+    """Take a first and last name and return a formatted name """
+
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
