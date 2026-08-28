b5df5f3dc897d395ed15e26786847ea277b4acce
Robert Pliszka <robplisz@gmail.com>
2026-08-28 19:44:31 -0400

    Completed game of blackjack

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,5 +1,6 @@
 # Blackjack game
 import random
+import art
 
 def deal_card():
     """Returns random card from the deck"""
@@ -18,34 +19,62 @@ def calculate_score(cards):
 
     return sum(cards)
 
-user_cards = []
-computer_cards = []
-computer_score = -1
-user_score = -1
-game_over = False
-
-# Adds 2 cards to user and computer cards
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-while not game_over:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-
-    print(f"Your cards: {user_cards}, current score: {user_score}")
-    print(f"Computer's first card: {computer_cards[0]}")
-
-
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-        game_over = True
+def compare(u_score, c_score):
+    """Takes user_score and computer_score and returns True if they win or lose"""
+    if u_score == c_score:
+        return "Draw"
+    elif c_score == 0:
+        return "Lose, opponent has a Blackjack"
+    elif u_score == 0:
+        return "Win with a Blackjack"
+    elif u_score > 21:
+        return "You went over. You lose"
+    elif c_score > 21:
+        return "Opponent went over. You win"
+    elif u_score > c_score:
+        return "You win"
     else:
-        user_deal = input("Type 'y' for another card, type 'n' to pass: ")
-        if user_deal == "y":
-            user_cards.append(deal_card())
-        else:
-            game_over = True
+        return "You lose"
 
-while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_card())
-    computer_score = calculate_score(computer_cards)
+def play_game():
+    """Play a game of Blackjack"""
+    print(art.logo)
+    user_cards = []
+    computer_cards = []
+    computer_score = -1
+    user_score = -1
+    game_over = False
+
+    # Adds 2 cards to user and computer cards
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            game_over = True
+        else:
+            user_deal = input("Type 'y' for another card, type 'n' to pass: ")
+            if user_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand is {user_cards}, final score is {user_score}")
+    print(f"Computer's final hand: {computer_cards}, final score is {computer_score}")
+    print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    print("\n" * 20)
+    play_game()
