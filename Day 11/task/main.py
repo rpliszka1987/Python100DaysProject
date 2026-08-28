fb818f09113784eba65e316f59353ae9ad399a43
Robert Pliszka <robplisz@gmail.com>
2026-08-28 19:26:42 -0400

    Created while loops to add cards to player deck and add cards to comp deck while score under 17

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -20,6 +20,8 @@ def calculate_score(cards):
 
 user_cards = []
 computer_cards = []
+computer_score = -1
+user_score = -1
 game_over = False
 
 # Adds 2 cards to user and computer cards
@@ -27,12 +29,23 @@ for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+while not game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
 
-print(f"Your cards: {user_cards}, current score: {user_score}")
-print(f"Computer's first card: {computer_cards[0]}")
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
 
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    game_over = True
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        game_over = True
+    else:
+        user_deal = input("Type 'y' for another card, type 'n' to pass: ")
+        if user_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
