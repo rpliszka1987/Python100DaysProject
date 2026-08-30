2292067d9240fd2e1857a721982db1676bab6fda
Robert Pliszka <robplisz@gmail.com>
2026-08-30 14:07:26 -0400

    Found first bug in the code

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# for loop runs 20 times cause of the range
 # 2. When is the function meant to print "You got it"?
+# meant to print when the range number i gets to count of 20
 # 3. What are your assumptions about the value of i?
+# it runs from 1 to 20 but not including 21 cause range doesnt include last number
