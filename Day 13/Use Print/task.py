f720975d22f8fa23a3f34af37cc3c25515f43fee
Robert Pliszka <robplisz@gmail.com>
2026-08-31 20:59:03 -0400

    Fixed code with error

--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,7 @@
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
+print(word_per_page)
+print(pages)
 total_words = pages * word_per_page
 print(total_words)
